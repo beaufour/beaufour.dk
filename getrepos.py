c5b2ead7907636785905cebc327587507cf60058
Allan Beaufour <allan@beaufour.dk>
2013-04-09 20:43:40 -0400

    add script that generates code.html contents

---
+++ getrepos.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# Silly little script to download the list of my repos and print the
+# html. Need to be integrated into the build process of Cactus.
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import sys
+
+import requests
+
+
+def main():
+    repos = requests.get('https://api.github.com/users/beaufour/repos')
+
+    data = repos.json
+    out = []
+    for entry in data:
+        out.append('  <dt><a href="{0}">{1}</a></dt>'.format(entry['html_url'], entry['name']))
+        out.append('  <dd>{0}{1}</dd>'.format(entry['description'], " <i>(fork)</i>" if entry['fork'] else ""))
+        out.append('')
+    print('\n'.join(out))
+
+if __name__ == '__main__':
+    sys.exit(main())
