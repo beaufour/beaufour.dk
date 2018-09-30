d2e06149a516e3ee12af20378928c14c6761e264
Allan Beaufour <allan@beaufour.dk>
2018-09-30 12:58:14 -0400

    turn everything into one static file

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+DEST=$1
+if [ -z "$DEST" ]; then
+    DEST=test
+fi
+BUCKET=$DEST.beaufour.dk
+echo Upload to: $BUCKET
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+aws s3 sync --acl public-read --exclude ".git*" --exclude "upload.sh" . s3://$BUCKET/
